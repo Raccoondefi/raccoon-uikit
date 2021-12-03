2c9ba4ad0448561d257b16a4bdf8044d95c2c324
Raccoondefi <DelorasWisingeranc43@gmail.com>
2021-12-04 00:15:49 +0800

    rebrand

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,9 +2,9 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#aa8929",
-  primaryBright: "#aa8929",
-  primaryDark: "#aa8929",
+  primary: "#000000",
+  primaryBright: "#000000",
+  primaryDark: "#000000",
   secondary: "#7645D9",
   success: "#31D0AA",
   warning: "#FFB237",
@@ -23,9 +23,9 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#452A7A",
+  text: "#000000",
   textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
+  textSubtle: "#a8a8a8",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
@@ -36,6 +36,8 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
+  primary: "#9750F1",
+  primaryBright: "#9750F1",
   secondary: "#9A6AFF",
   background: "#343135",
   backgroundDisabled: "#3c3742",
@@ -44,7 +46,7 @@ export const darkColors: Colors = {
   input: "#483f5a",
   primaryDark: "#0098A1",
   tertiary: "#353547",
-  text: "#EAE2FC",
+  text: "#ffffff",
   textDisabled: "#666171",
   textSubtle: "#c9c4d4",
   borderColor: "#524B63",
